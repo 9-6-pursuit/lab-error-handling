ef0712d349fee2304d3d734837e25ebf3c262f1d
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-02-21 20:48:22 -0500

    Solved ERROR-HANDLING Lab

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+  if (!cart.length){
+    throw "The cart array is empty."
+  }
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -36,8 +39,20 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
+  if (products.length === 0) {
+    throw "Products array is empty"
+  }
+  if (typeof min !== "number" || typeof max !== "number") {
+    throw "Min or Max is not a number"
+  } 
+  if (min > max || max <= 0 || min < 0) {
+    throw "Min or Max has the worng value"
+  }
   const result = [];
   for (let product of products) {
+    if (product.priceInCents === undefined) {
+      throw `${product.name} has no priceInCents key`
+    }  
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,10 +64,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
+
+ try { 
   const filteredProducts = filterProductsByPriceRange(products, min, max);
   const total = getCartTotal(filteredProducts);
-
   return total;
+} catch (error) {
+  return 0
+}
 }
 
 module.exports = {
