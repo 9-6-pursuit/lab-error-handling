add0cc8c71bf18037e7795f01dfc5bea9745b3f7
Phillip Andrew Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-02-21 20:45:44 -0500

    DONE

--- index.js
+++ index.js
@@ -19,12 +19,16 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+  if (cart.length === 0) {
+    throw "The cart array is empty."
+  }
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
   }
   return result;
 }
+
 
 /*
   This function should throw an error if:
@@ -36,8 +40,27 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
+  if (products.length === 0) {
+    throw "The products array is empty."
+  }
+  if (typeof min !== "number" || typeof max !== "number") {
+    throw "Min and max values must be numbers."
+  }
+  if (min < 0 || max < 0) {
+    throw "Min and max values are less than 0"
+  }
+  if (max === 0) {
+    throw "Max is equal to 0"
+  }
   const result = [];
   for (let product of products) {
+    // if (!products.priceInCents){
+    //   throw `${product.name} has no priceInCents key.`
+    // } alternate solution I made
+    if (product.priceInCents === undefined ){
+      throw `${product.name} has no priceInCents key.`
+    }
+    // } you have to account for if a products price is 0
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,10 +72,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+    try { 
+      const filteredProducts = filterProductsByPriceRange(products, min, max);
+      const total = getCartTotal(filteredProducts);
+      
+    return total
+  } catch (error) {
+    return 0;
+  }
 }
 
 module.exports = {
