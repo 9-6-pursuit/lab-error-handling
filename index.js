f797a940e2666e10484954521c1cf55be04d9bc0
salvagebar <salvagebar@gmail.com>
2023-04-13 21:44:56 -0400

    All tests pass

--- index.js
+++ index.js
@@ -39,13 +39,21 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
-  if (!products.length || !min || !max || min < 0 || max < 0 || typeof min !== "number" || typeof max !== "number") {
+  if (
+    !products.length ||
+    !min ||
+    !max ||
+    min < 0 ||
+    max < 0 ||
+    typeof min !== "number" ||
+    typeof max !== "number"
+  ) {
     throw "Error";
   }
   const result = [];
   for (let product of products) {
     if (!product.priceInCents) {
-      throw "Error: Product missing price"
+      throw "Error: Product missing price";
     }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
@@ -58,10 +66,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
 
-  return total;
+    return total;
+  } catch (error) {
+    return 0;
+  }
 }
 
 module.exports = {
