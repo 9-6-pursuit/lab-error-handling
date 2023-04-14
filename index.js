b09e2f70f681982f045d10b78cc7dc23cda86ae7
salvagebar <salvagebar@gmail.com>
2023-04-13 21:39:17 -0400

    Two functions pass

--- index.js
+++ index.js
@@ -39,11 +39,14 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
-  if (!products || !min || !max) {
+  if (!products.length || !min || !max || min < 0 || max < 0 || typeof min !== "number" || typeof max !== "number") {
     throw "Error";
   }
   const result = [];
   for (let product of products) {
+    if (!product.priceInCents) {
+      throw "Error: Product missing price"
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
